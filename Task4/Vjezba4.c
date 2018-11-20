29884004125958f9d99e4aa363ed5d2ea52d25ba
Josip Volarević <34980162+Volki312@users.noreply.github.com>
2018-11-20 22:11:14 +0100

    Code optimization
    Used measure( ) from sortiranja.h

--- Task4/Vjezba4.c
+++ Task4/Vjezba4.c
@@ -17,34 +17,28 @@ void main() {
 	scanf("%d", &n);
 
 	for(int i = 0; i < 4; i++) {
-	// generiramo niz 4 puta, prvi i treći puta bez pivota, treći i četvrti puta sa već sortiranim nizom
-        niz = generate(n);
-        if (i > 1) quicksort(niz, n);
+		// generiramo niz 4 puta, prvi i treći puta bez pivota, treći i četvrti puta sa već sortiranim nizom
+        	niz = generate(n);
+        	if (i > 1) quicksort(niz, n);
+        	(i % 2) == 1 ? (choose_pivot = true) : (choose_pivot = false);
 
-        (i % 2) == 1 ? (choose_pivot = true) : (choose_pivot = false);
-
-        start_t = clock();
-        quicksort(niz, n);
-        end_t = clock();
-
-        switch(i){
-            case 0:
-                printf("Vrijeme izvodenja quicksorta nasumicnog niza bez odabira pivota: ");
-                break;
-            case 1:
-                printf("Vrijeme izvodenja quicksorta nasumicnog niza sa odabirom pivota: ");
-                break;
-            case 2:
-                printf("Vrijeme izvodenja quicksorta sortiranog niza bez odabira pivota: ");
-                break;
-            case 3:
-                printf("Vrijeme izvodenja quicksorta sortiranog niza sa odabirom pivota: ");
-                break;
-            default:
-                printf("U WuT M8?!");
-                break;
-        }
-        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-        printf("%f\n", total_t);
+        	switch(i){
+            		case 0:
+                		printf("Vrijeme izvodenja quicksorta nasumicnog niza bez odabira pivota: ");
+                		break;
+            		case 1:
+                		printf("Vrijeme izvodenja quicksorta nasumicnog niza sa odabirom pivota: ");
+                		break;
+            		case 2:
+                		printf("Vrijeme izvodenja quicksorta sortiranog niza bez odabira pivota: ");
+                		break;
+            		case 3:
+                		printf("Vrijeme izvodenja quicksorta sortiranog niza sa odabirom pivota: ");
+                		break;
+            		default:
+                		printf("U WuT M8?!");
+                		break;
+        	}
+		printf("%f\n", measure(quicksort, niz, len));
 	}
 }
