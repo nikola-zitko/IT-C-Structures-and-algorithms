785820e8fc00d08a15ab685d7a1d83e74f8d1e9f
Josip Volarević <34980162+Volki312@users.noreply.github.com>
2018-11-20 21:50:12 +0100

    main.c update
    First part of the task4 finished

--- Task4/Vjezba4.c
+++ Task4/Vjezba4.c
@@ -1,33 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<stdbool.h>
 #include "sortiranja.h"
 
 
 
 void main() {
 
-	int* niz;
+	int *niz;
 	int n;
 	clock_t start_t, end_t;
+	double total_t;
 
-	printf("Unesi duljinu niza: ");
+	printf("Unesite zeljenu duljinu niza: ");
 	scanf("%d", &n);
 
-	choose_pivot = false;
-	start_t = clock();
-	niz = generate(n);
-	//print(niz, n);
-	partition(niz, n);
-	end_t = clock();
-	printf("Vrijeme izvodenja: %f\n", ((float)(end_t - start_t) / CLOCKS_PER_SEC));
+	for(int i = 0; i < 4; i++) {
+	// generiramo niz 4 puta, prvi i treći puta bez pivota, treći i četvrti puta sa već sortiranim nizom
+        niz = generate(n);
+        if (i > 1) quicksort(niz, n);
 
-	choose_pivot = true;
-	start_t = clock();
-	niz = generate(n);
-	partition(niz, n);
-	end_t = clock();
-	printf("Vrijeme izvodenja sa pivotom: %f\n", ((float)(end_t - start_t) / CLOCKS_PER_SEC));
+        (i % 2) == 1 ? (choose_pivot = true) : (choose_pivot = false);
 
+        start_t = clock();
+        quicksort(niz, n);
+        end_t = clock();
 
+        switch(i){
+            case 0:
+                printf("Vrijeme izvodenja quicksorta nasumicnog niza bez odabira pivota: ");
+                break;
+            case 1:
+                printf("Vrijeme izvodenja quicksorta nasumicnog niza sa odabirom pivota: ");
+                break;
+            case 2:
+                printf("Vrijeme izvodenja quicksorta sortiranog niza bez odabira pivota: ");
+                break;
+            case 3:
+                printf("Vrijeme izvodenja quicksorta sortiranog niza sa odabirom pivota: ");
+                break;
+            default:
+                printf("U WuT M8?!");
+                break;
+        }
+        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+        printf("%f\n", total_t);
+	}
 }
